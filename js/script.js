318b0f3cef1a173eb3b13971e2cea5953d06de82
LeonardoFlammini <leonardo98flammini@gmail.com>
2023-09-15 15:57:13 +0200

    [add] script

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const spesaOut = document.getElementById("spesa");
+let i = 0;
+const list = [
+  carciofi,
+  mele,
+  peperoni,
+  pollo,
+  gelato
+]
+
+while (i < list.length ) {
+  const item = list[i] ;
+  
+}
