cb8778b17e09a4d7895b7633b75052190dd83ec2
LeonardoFlammini <leonardo98flammini@gmail.com>
2023-09-15 16:03:41 +0200

    finito

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 const spesaOut = document.getElementById("spesa");
 let i = 0;
 const list = [
-  carciofi,
-  mele,
-  peperoni,
-  pollo,
-  gelato
+  "carciofi",
+  "mele",
+  "peperoni",
+  "pollo",
+  "gelato"
 ]
 
 while (i < list.length ) {
-  const item = list[i] ;
-  
+  const item = list[i];
+  spesaOut.innerHTML += "<li class=\"list-group-item\">" + item + "</li>";
+  i++;
 }
